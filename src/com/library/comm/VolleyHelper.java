fd9542b76e47054f70aa0c5b86b6cc3590310ece
Administrator <Administrator@lx-PC>
2014-12-06 21:36:35 +0800

    Signed-off-by: Administrator <Administrator@lx-PC>

--- src/com/library/comm/VolleyHelper.java
+++ src/com/library/comm/VolleyHelper.java
@@ -1,17 +1,30 @@
 package com.library.comm;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.support.v4.util.LruCache;
+import android.util.Log;
+import android.util.LruCache;
 
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
+import com.android.volley.Request.Method;
 import com.android.volley.RequestQueue;
 import com.android.volley.RequestQueue.RequestFilter;
+import com.android.volley.Response;
+import com.android.volley.Response.ErrorListener;
+import com.android.volley.Response.Listener;
 import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.library.core.LruBitmapCache;
 
 public class VolleyHelper {
+	private static final String TAG = "VolleyHelper";
 	private static VolleyHelper mInstance;
 	private RequestQueue mRequestQueue;
 	private ImageLoader mImageLoader;
@@ -21,8 +34,9 @@ public class VolleyHelper {
 		mCtx = context;
 		mRequestQueue = getRequestQueue();
 		mImageLoader = new ImageLoader(mRequestQueue, new ImageLoader.ImageCache() {
-//			private final LruCache<String, Bitmap> cache = new LruCache<String, Bitmap>(20);
+			//			private final LruCache<String, Bitmap> cache = new LruCache<String, Bitmap>(20);
 			private final LruCache<String, Bitmap> cache = new LruBitmapCache(context.getApplicationContext());
+
 			@Override
 			public Bitmap getBitmap(String url) {
 				return cache.get(url);
@@ -58,13 +72,35 @@ public class VolleyHelper {
 	public ImageLoader getImageLoader() {
 		return mImageLoader;
 	}
-	
-	public void cancelAll(Object requestTag){
+
+	public void cancelAll(Object requestTag) {
 		mRequestQueue.cancelAll(requestTag);
 	}
-	
-	public void cancelAll(RequestFilter requestFilter){
+
+	public void cancelAll(RequestFilter requestFilter) {
 		mRequestQueue.cancelAll(requestFilter);
+	}
+
+	public void postJson(String url, String requestBody, Listener<JSONObject> listener, ErrorListener errorListener) {
+		JSONObject jsonObject;
+		try {
+			jsonObject = new JSONObject(requestBody);
+			addToRequestQueue(new JsonObjectRequest(Method.POST, url, jsonObject, listener, errorListener));
+		}
+		catch (JSONException e) {
+			// TODO Auto-generated catch block
+			Log.e(TAG, e.getMessage());
+			e.printStackTrace();
+		}
+
+	}
+
+	public void getJson(String url, Listener<JSONObject> listener, ErrorListener errorListener) {
+		addToRequestQueue(new JsonObjectRequest(Method.GET, url, null, listener, errorListener));
+	}
+	
+	public void getString(String url, Listener<String> listener, ErrorListener errorListener){
+		addToRequestQueue(new StringRequest(url, listener, errorListener));
 	}
 
 }

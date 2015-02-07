af299b3718e349d8850cd94ab5b42130f720a432
Administrator <Administrator@lx-PC>
2015-02-07 14:19:04 +0800

    Signed-off-by: Administrator <Administrator@lx-PC>

--- src/com/library/util/LibUtil.java
+++ src/com/library/util/LibUtil.java
@@ -1,13 +1,21 @@
 package com.library.util;
 
 import java.lang.reflect.Field;
+import java.util.List;
 
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningAppProcessInfo;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.graphics.Rect;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Environment;
 import android.telephony.TelephonyManager;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewTreeObserver.OnGlobalLayoutListener;
 
 public class LibUtil {
 	/**
@@ -147,6 +155,47 @@ public class LibUtil {
 		else
 			return false;
 	}
+	
+	/**
+	 * 网络是否已连接
+	 * 
+	 * @param context
+	 * @return
+	 */
+	public static boolean isNetworkConnected(Context context) {
+		NetworkInfo networkInfo = ((ConnectivityManager) context
+				.getSystemService(Context.CONNECTIVITY_SERVICE))
+				.getActiveNetworkInfo();
+		return networkInfo != null && networkInfo.isConnected();
+	}
+	
+	/**
+	 * Cmwap网络是否已连接
+	 * 
+	 * @param context
+	 * @return
+	 */
+	public static boolean isNetworkConnectedByCmwap(Context context) {
+		NetworkInfo networkInfo = ((ConnectivityManager) context
+				.getSystemService(Context.CONNECTIVITY_SERVICE))
+				.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+		return networkInfo != null && networkInfo.isConnected()
+				&& networkInfo.getExtraInfo() != null
+				&& networkInfo.getExtraInfo().toLowerCase().contains("cmwap");
+	}
+	
+	/**
+	 * Wifi网络是否已连接
+	 * 
+	 * @param context
+	 * @return
+	 */
+	public static boolean isNetworkConnectedByWifi(Context context) {
+		NetworkInfo networkInfo = ((ConnectivityManager) context
+				.getSystemService(Context.CONNECTIVITY_SERVICE))
+				.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+		return networkInfo != null && networkInfo.isConnected();
+	}
 
 	/**
 	 * 
@@ -164,7 +213,7 @@ public class LibUtil {
 		TelephonyManager manager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
 		return manager.getDeviceId();
 	}
-	
+
 	/**
 	 * 
 	 * 描          述 ：获取本机手机号码
@@ -177,11 +226,11 @@ public class LibUtil {
 	 * @return
 	 *
 	 */
-	public static String getPhoneNumber(Context context){
-		TelephonyManager telephonyManager=(TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+	public static String getPhoneNumber(Context context) {
+		TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
 		String simSerialNum = telephonyManager.getLine1Number();
-		if(!simSerialNum.isEmpty())
-			return simSerialNum.substring(simSerialNum.length()-11);
+		if (!simSerialNum.isEmpty())
+			return simSerialNum.substring(simSerialNum.length() - 11);
 		return simSerialNum;
 	}
 
@@ -235,7 +284,7 @@ public class LibUtil {
 
 	/**
 	 * 
-	 * 描          述 ：dp单位转化为px
+	 * 描          述 ：dp单位转化为px(系统自带处理方式)
 	 * 创建日期  : 2014-11-30
 	 * 作           者 ： lx
 	 * 修改日期  : 
@@ -246,14 +295,30 @@ public class LibUtil {
 	 * @return
 	 *
 	 */
-	public static int convertToPx(Context context, float dp) {
-		final float scale = context.getResources().getDisplayMetrics().density;
-		return (int) (dp * scale + 0.5f);
+	public static float convertDpToPx(Context context, float dp) {
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.getResources().getDisplayMetrics());
+	}
+	
+	/**
+	 * 
+	 * 描          述 ：sp单位转化为px(系统自带处理方式)
+	 * 创建日期  : 2014-11-30
+	 * 作           者 ： lx
+	 * 修改日期  : 
+	 * 修   改   者 ：
+	 * @version   : 1.0
+	 * @param context
+	 * @param dp
+	 * @return
+	 *
+	 */
+	public static float convertSpToPx(Context context, float sp) {
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp, context.getResources().getDisplayMetrics());
 	}
 
 	/**
 	 * 
-	 * 描          述 ：px单位转化为dp
+	 * 描          述 ：px单位转化为dp（系统处理方式）
 	 * 创建日期  : 2014-11-30
 	 * 作           者 ： lx
 	 * 修改日期  : 
@@ -264,9 +329,89 @@ public class LibUtil {
 	 * @return
 	 *
 	 */
-	public static int convertToDp(Context context, float px) {
+	public static float convertPxToDp(Context context, float px) {
 		final float scale = context.getResources().getDisplayMetrics().density;
-		return (int) (px / scale + 0.5f);
+		return px / scale;
 	}
+
+	/**将软键盘遮盖的部分推至顶部
+	 * @param root
+	 *            最外层布局，需要调整的布局
+	 * @param scrollToView
+	 *            被键盘遮挡的scrollToView，滚动root,使scrollToView在root可视区域的底部
+	 */
+	private void controlKeyboardLayout(final View root, final View scrollToView) {
+		root.getViewTreeObserver().addOnGlobalLayoutListener(new OnGlobalLayoutListener() {
+			@Override
+			public void onGlobalLayout() {
+				Rect rect = new Rect();
+				// 获取root在窗体的可视区域
+				root.getWindowVisibleDisplayFrame(rect);
+				// 获取root在窗体的不可视区域高度(被其他View遮挡的区域高度)
+				int rootInvisibleHeight = root.getRootView().getHeight() - rect.bottom;
+				// 如果内容被遮挡则将内容上推至软键盘顶部
+				if (scrollToView.getBottom() > rect.bottom) {
+					int[] location = new int[2];
+					// 获取scrollToView在窗体的坐标
+					scrollToView.getLocationInWindow(location);
+					// 计算root滚动高度，使scrollToView在可见区域
+					int srollHeight = (location[1] + scrollToView.getHeight()) - rect.bottom;
+					root.scrollTo(0, srollHeight);
+				}
+				else {
+					// 键盘隐藏
+					root.scrollTo(0, 0);
+				}
+			}
+		});
+	}
+	
+	/**
+	 * 获取版本名称
+	 * 
+	 * @param context
+	 * @return
+	 */
+	public static String getVersionName(Context context) {
+		String versionName = "-1";
+		try {
+			versionName = context.getPackageManager().getPackageInfo(
+					context.getPackageName(), 0).versionName;
+		} catch (NameNotFoundException e) {
+			e.printStackTrace();
+		}
+		return versionName;
+	}
+	
+	/**
+	 * 根据包名判断应用是否在前台运行
+	 * 
+	 * @param context
+	 * @param packageName
+	 * @return
+	 */
+	public static boolean isAppOnForeground(Context context, String packageName) {
+		if (packageName != null) {
+			// Returns a list of application processes that are running on the
+			// device
+			ActivityManager activityManager = (ActivityManager) context
+					.getSystemService(Context.ACTIVITY_SERVICE);
+			List<RunningAppProcessInfo> appProcesses = activityManager
+					.getRunningAppProcesses();
+			if (appProcesses != null) {
+				for (RunningAppProcessInfo appProcess : appProcesses) {
+					// The name of the process that this object is associated
+					// with.
+					if (appProcess.processName.equals(packageName)
+							&& RunningAppProcessInfo.IMPORTANCE_FOREGROUND == appProcess.importance) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+	
+	
 
 }

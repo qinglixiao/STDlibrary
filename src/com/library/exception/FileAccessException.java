2eddbf069a94baf0a4fe9e3c94fd1f5afef08786
137289 <lixiao3@syswin.com>
2016-03-28 13:41:54 +0800

    修改文件字符编码

--- src/com/library/exception/FileAccessException.java
+++ src/com/library/exception/FileAccessException.java
@@ -1,81 +1,32 @@
 package com.library.exception;
-/**
- * 
- * ��          �� ���ļ���ȡ�쳣��
- * �� �� �� ��  : 2013-6-24
- * ��          �� �� lx
- * �� �� �� ��  : 
- * ��   ��   �� ��
- * @version   : 1.0
- */
+
 public class FileAccessException extends BaseSTDException{
 	private static final long serialVersionUID = 1L;
 	
-	/**
-	 * ��ʼ���飬��ʼ���Զ����쳣������Ϣ
-	 */
+
 	{
 		this.mID = "5";
-		this.mMessage = "�ļ���ȡ�쳣��";
-		this.mDescription = "·�����޴��ļ����޸��ļ��ķ���Ȩ��";
+		this.mMessage = "";
+		this.mDescription = "";
 	}
 	
-	/**
-	 * 
-	 * ��          �� ���޲ι���
-	 * �� �� �� ��  : 2013-6-25
-	 * ��          �� �� lx
-	 * �� �� �� ��  : 
-	 * ��   ��   �� ��
-	 * @version   : 1.0
-	 *
-	 */
+
 	public FileAccessException() {
 		super();
 	}
 	
-	/**
-	 * 
-	 * ��          �� ���вι���
-	 * �� �� �� ��  : 2013-6-25
-	 * ��          �� �� lx
-	 * �� �� �� ��  : 
-	 * ��   ��   �� ��
-	 * @version   : 1.0
-	 * @param msg �û���ʾ�Ѻ���Ϣ
-	 *
-	 */
+
 	public FileAccessException(String msg) {
 		super(msg);
 	}
 
-	/**
-	 * 
-	 * ��          �� ���вι���
-	 * �� �� �� ��  : 2013-6-25
-	 * ��          �� �� lx
-	 * �� �� �� ��  : 
-	 * ��   ��   �� ��
-	 * @version   : 1.0
-	 * @param ex ϵͳ�쳣�׳�����
-	 *
-	 */
+
+
 	public FileAccessException(Throwable ex) {
 		super(ex);
 	}
 	
-	/**
-	 * 
-	 * ��          �� ���вι���
-	 * �� �� �� ��  : 2013-6-25
-	 * ��          �� �� lx
-	 * �� �� �� ��  : 
-	 * ��   ��   �� ��
-	 * @version   : 1.0
-	 * @param msg �û���ʾ�Ѻ���Ϣ
-	 * @param ex ϵͳ�쳣�׳�����
-	 *
-	 */
+
 	public FileAccessException(String msg, Throwable ex) {
 		super(msg, ex);
 	}

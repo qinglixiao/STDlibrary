70bba505008e41c76a20b3faf419bd87504a8b8c
137289 <lixiao3@syswin.com>
2016-03-29 14:26:46 +0800

    添加图片处理方法

--- src/com/library/util/About.java
+++ src/com/library/util/About.java
@@ -17,4 +17,13 @@ public class About {
 	 */
 	public static int API_INT = Build.VERSION.SDK_INT;
 
+	/**
+	 * 本机版本是否高于API14
+	 * (对应Android 4.0)
+	 * @return
+	 */
+	public static boolean aboveApi14(){
+		return API_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH;
+	}
+
 }

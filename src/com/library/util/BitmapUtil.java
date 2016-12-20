6fd16bcd2d1bb82caaca852912438a2603805780
qinglixiao <qinglixiao@163.com>
2016-12-20 22:10:32 +0800

    update

--- src/com/library/util/BitmapUtil.java
+++ src/com/library/util/BitmapUtil.java
@@ -3,11 +3,15 @@ package com.library.util;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.text.TextUtils;
 import android.util.Size;
 
+import java.io.IOException;
+
 /**
- * 图片操作工具类，用于图片压缩处理
+ * 图片操作工具类
  * Created by gfy on 2016/3/29.
  */
 public class BitmapUtil {
@@ -78,7 +82,7 @@ public class BitmapUtil {
         return inSampleSize;
     }
 
-    public int[] getImageSize(String imagePath) {
+    public static int[] getImageSize(String imagePath) {
         int[] size = new int[2];
         if (!TextUtils.isEmpty(imagePath)) {
             BitmapFactory.Options options = new BitmapFactory.Options();
@@ -90,4 +94,62 @@ public class BitmapUtil {
         }
         return size;
     }
+
+    /**
+     * 读取图片的旋转的角度
+     *
+     * @param path 图片绝对路径
+     * @return 图片的旋转角度
+     */
+    public static int getBitmapDegree(String path) {
+        int degree = 0;
+        try {
+            // 从指定路径下读取图片，并获取其EXIF信息
+            ExifInterface exifInterface = new ExifInterface(path);
+            // 获取图片的旋转信息
+            int orientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                    ExifInterface.ORIENTATION_NORMAL);
+            switch (orientation) {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    degree = 90;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    degree = 180;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    degree = 270;
+                    break;
+            }
+        } catch (IOException e) {
+        }
+        return degree;
+    }
+
+    /**
+     * 将图片按照某个角度进行旋转
+     *
+     * @param bm     需要旋转的图片
+     * @param degree 旋转角度
+     * @return 旋转后的图片
+     */
+    public static Bitmap rotateBitmapByDegree(Bitmap bm, int degree) {
+        Bitmap returnBm = null;
+        // 根据旋转角度，生成旋转矩阵
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degree);
+        try {
+            // 将原始图片按照旋转矩阵进行旋转，并得到新的图片
+            returnBm = Bitmap.createBitmap(bm, 0, 0, bm.getWidth(), bm.getHeight(), matrix, true);
+        } catch (OutOfMemoryError e) {
+        } finally {
+            if (returnBm == null) {
+                returnBm = bm;
+            }
+            if (bm != returnBm) {
+                bm.recycle();
+            }
+        }
+        return returnBm;
+    }
+
 }
